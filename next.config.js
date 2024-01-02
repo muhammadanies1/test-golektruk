72745fac2795d515b88db66d854edca5e9ef27bb
Muhammad Anies Wahdie <manies5396@gmail.com>
2024-01-03 01:47:49 +0700

    feat: design login done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'golektruk.com',
+        port: '',
+        pathname: '/login.png',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
